ef09a458970ac1b201fd341e87f00fd9b8347130
MD Maruf Prodhan <mdmaruf.shadhin@gmail.com>
2024-08-10 23:25:36 +0600

    Initial Commit

---
+++ quizeApp/quizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizeAppApp.swift
+//  quizeApp
+//
+//  Created by Maruf on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct quizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
